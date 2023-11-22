93c528d63a6e74f2cd030c2f23a8289de69a75f4
hussain-ahusan <hussainahusan902@hotmail.com>
2023-11-22 23:47:19 +0500

    completed 2nd task remove from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,9 @@
-const removeFromArray = function(...args) {
-    // use ...args to make the function accept any number of arguments
-    // create a variable to hold the first array argument
-    // create an empty array variable to push the result of the remove operation
-    // use the array special forEach loop to loop through the array
-    // check if args includes the items to be removed.
-    // after the evaluation, push the results of the evaluation to the empty array created beforehand.
-    const array = args[0]
-    const newArray = []
-    array.forEach( (item) => {
-        if (!args.includes(item)) {
-            newArray.push(item)
+const removeFromArray = function(givenArray, ...args) {
+
+    const newArray = new Array();
+    givenArray.forEach( (element) => {
+        if (!args.includes(element)) {
+            newArray.push(element)
         }
     })
     return newArray
