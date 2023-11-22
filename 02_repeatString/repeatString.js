26b6a89ec00060cbb7d01d1aba6a26809b3cd7e4
hussain-ahusan <hussainahusan902@hotmail.com>
2023-11-22 18:25:28 +0500

    completed the 1st task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,3 +1,5 @@
+/*
+THIS CODE WAS WRITTEN FROM A YT TUTORIAL
 const repeatString = function(message, loops) {
     //using switch statement, check is number of loops is less than 0, if it is then return error, if it is 0, then return an empty string
     // create a variable to hold the message
@@ -19,6 +21,26 @@ const repeatString = function(message, loops) {
     }
     return greeting
 };
+*/
+
+//This is my own code written to meet all the test conditions
+function repeatString(greetingMessage, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    let greeting = '';
+    let i=0;
+    while (i < repetitions) {
+        greeting += greetingMessage;
+
+    i++
+    };
+
+return greeting;
+};
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
