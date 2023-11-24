53dbdc76ad43e1f1c8e83ba110fadcfc81a39db1
hussain-ahusan <hussainahusan902@hotmail.com>
2023-11-24 14:42:15 +0500

    Task completed and simplified for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,23 +1,21 @@
 // convert temp to celsius
-// create a variable to hold the fahrenheit temp
 // create a variable to hold the conversion formulae
-// create a variable to hold the converted temp rounded off to one decimal place
-//return the one dp variable
+// create a result variable to hold the converted temp rounded off to one decimal place
+//return the result
 const convertToCelsius = function(fahrenheit) {
-  let fahrenheitTemp = fahrenheit
-  let FahrenheitToCelsius = (fahrenheitTemp - 32) * 5/9
+
+  let FahrenheitToCelsius = (fahrenheit - 32) * 5/9
   let result = Math.round(FahrenheitToCelsius * 10)/10
   return result
 };
 
 // convert temp to fahrenheit
-// create a variable to hold fahrenheit temp
 // create a variable to hold the conversion formulae
-// create a variable to hold the converted temp rounded off to one decimal place
-// return the one dp variable 
+// create a result variable to hold the converted temp rounded off to one decimal place
+// return the result
 const convertToFahrenheit = function(celsius) {
-    let celsiusTemp = celsius
-    let celsiusToFahrenheit = celsiusTemp * 9/5 + 32 
+    
+    let celsiusToFahrenheit = celsius * 9/5 + 32 
     let result = Math.round(celsiusToFahrenheit * 10)/10
     return result
 };
