d86f0256b285c4e4059515bbb0824fb209f29617
hussain-ahusan <hussainahusan902@hotmail.com>
2023-11-28 18:36:22 +0500

    Task complete for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,54 +1,61 @@
 const add = function(num1, num2) {
-  // create a variable to hold addition operation
-  // return the addition variable
-    let ans = num1 +num2
-    return ans
+  let sum = num1 + num2;
+  return sum;
 };
 
 const subtract = function(num1, num2) {
-  // create a variable to hold the subtraction operation
-  // return the subtraction variable
-    let ans = num1- num2
-    return ans
+  let subtracts = num1 - num2;
+  return subtracts;
 };
 
-const sum = function(array) {
+const sum = function(passedArray) {
   // create a variable to hold the sum operation
   // use array.reduced method so that it can add many numbers and return the final answer
-  // return the sum variable
-    let ans = array.reduce( function (accumulator, nextItem){
-      return nextItem + accumulator
-    },0);
-    return ans
+  // return the ans
+
+  let answer = passedArray.reduce((firstNum, nextNum) => {
+    return firstNum + nextNum;
+  },0)
+
+  return answer;
 };
 
-const multiply = function(arr) {
-  // create a variable to hold the multiplication operation
-  // use array.reduce method so that it can multiply many numbers and return the final answer
-  // return the multiplication variable
-    let ans = arr.reduce((accumulator, nextItem) => accumulator * nextItem, 1)
-    return ans
+const multiply = function(passedArray) {
+  //declare a variable to hold the answer
+  //in that variable, use array.reduce method so that we can pass in as many numbers and calculate multipilication.
+  //return the final anwer
+  let answer = passedArray.reduce((firstNum, nextNum) => firstNum * nextNum,1);
+
+  return answer;
 };
 
 const power = function(num1, num2) {
-  // create a variable to hold the final answer
-  // use Math.pow method to raise the given number to the power of the second given number
-  // return the final answer variable
-    let ans = Math.pow(num1, num2)
-    return ans
+  let answer = Math.pow(num1, num2);
+  return answer;
 };
 
 const factorial = function(number) {
-  // check if the given number is less than 0, if it is then return error
-  // check if the given number is 0, if it is return 1
-  // otherwise return a variable that holds the expression used to find the factorialized number
-  if (number < 0) {
-    return "error"
-  } else if (number === 0) {
-    return 1
+  //create a variable to hold the passed number
+  //check if number is 0 or 1, if so return 1
+  //else use while loop to iterate through the given number while number is less than 2.
+  /*for each iteration, let number variable be equal to number variable times passed number minus 1. 
+  (note that passed number is same as number variable, it's the same as n=n*(n-1) )
+  */
+  let ans = number;
+
+  if (number === 0 || number === 1){
+    return 1;
   } else {
-	return ans = number * factorial(number - 1)
+    while (number > 2) {
+      ans = ans * (number -1)
+      number--;
+    }
+
+    return ans;
   }
+
+
+  
 };
 
 // Do not edit below this line
